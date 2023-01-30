89f247675c3ec9243148684d7efa98e789d3c52f
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-30 07:00:49 +0100

    Day 7

--- src/entities/concretes/Audi.java
+++ src/entities/concretes/Audi.java
@@ -9,26 +9,19 @@ public class Audi extends Cars {
 
     public List<Audi> audiList = new ArrayList<>();
 
-    private String model;
+   // private String model;
 
     public Audi() {
     }
 
-    public Audi(String model, String id, int modelYear, boolean gear, String fuelType, double dailyPrice) {
-        this.model = model;
+    public Audi(String model, String id, int modelYear, boolean gear, String fuelType, double dailyPrice,boolean isActive) {
+        super.setModel(model);
         super.setId(id);
         super.setModelYear(modelYear);
         super.setGear(gear);
         super.setFuelType(fuelType);
         super.setDailyPrice(dailyPrice);
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
+        super.setActive(isActive);
     }
 
     @Override
@@ -39,34 +32,20 @@ public class Audi extends Cars {
                         ", Model Year: "+ getModelYear() +
                         ", Automatic Gear: "+ isGear() +
                         ", Fuel Type: " + getFuelType() +
-                        ", Daily Price: " + getDailyPrice()
+                        ", Daily Price: " + getDailyPrice()+
+                        ", Is Active: " + isActive()
                 ;
     }
 
-    @Override
-    public void showAvailableCars(boolean isActive) {
-
-    }
-
-    @Override
-    public void showSelectedCar(boolean isActive) {
-
-    }
-
-    @Override
-    public void showInactiveCars(boolean isActive) {
-
-    }
-
     public void fillAudiList(){
-        Audi audi = new Audi();
-        Audi audi2 = new Audi();
-        Audi audi3 = new Audi();
-        Audi audi4 = new Audi();
-        Audi audi5 = new Audi();
-        Audi audi6 = new Audi();
-        Audi audi7 = new Audi();
-        Audi audi8 = new Audi();
+        Audi audi = new Audi("A1","AA12020100",2020,true,"gasoline",75.90,true);
+        Audi audi2 = new Audi("A1","AA12022101",2022,false,"electric",55.90,true);
+        Audi audi3 = new Audi("A2","AA22021100",2021,true,"diesel",65.90,true);
+        Audi audi4 = new Audi("A3","AA32022100",2022,true,"gasoline",65.90,true);
+        Audi audi5 = new Audi("A4","AA42022100",2022,false,"hybrid",75.90,true);
+        Audi audi6 = new Audi("A5","AA52023100",2023,true,"gasoline",85.90,true);
+        Audi audi7 = new Audi("Q7","AQ72021100",2021,true,"diesel",95.90,false);
+        Audi audi8 = new Audi("Q7","AQ72020101",2020,true,"electric",105.90,true);
         audiList.add(audi);
         audiList.add(audi2);
         audiList.add(audi3);
@@ -77,14 +56,46 @@ public class Audi extends Cars {
         audiList.add(audi8);
 
     }
+    @Override
+    public void showAvailableCars(boolean isActive){
+        System.out.println();
+        System.out.println("////////////////////////////////////// MÜSAIT ARACLARIMIZ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ \n");
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","Arac Kodu","Marka","Model","Model Yılı","Otomatik Vites","Yakıt Tipi","Günlük Fiyat");
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","-----------","--------","---------","----------","--------------","----------","------------");
+        for (Audi w :audiList){
 
-    public void showAudiCars(){
-
-        System.out.printf("%-7s  %-10s  %-10s  %-10s  %-14s  %-10s %-15s\n","Marka","Model","ID","Model Yılı","Otomatik Vites","Yakıt Tipi","Günlük Fiyat");
-        System.out.printf("%-7s  %-10s  %-10s  %-10s  %-14s  %-10s %-15s\n","-----","-----","--","----------","--------------","----------","------------");
-        for (Audi w:audiList){
-            System.out.printf("%-7s  %-10s  %-10s  %-10s  %-14s  %-10s $%-14s\n","Audi",w.getModel(),w.getId(),w.getModelYear(),w.isGear(),w.getFuelType(),w.getDailyPrice());
+            if (w.isActive()==isActive){
+                System.out.printf("%-13s  %-10s  %-16s  %-10s  %-12s  %-12s $%-14s\n",w.getId(),"Honda",w.getModel(),w.getModelYear(),w.isGear(),w.getFuelType(),w.getDailyPrice());
+            }
         }
+        System.out.println();
+    }
 
+    @Override
+    public void showSelectedCar(boolean isActive) {
+        System.out.println();
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","Arac Kodu","Marka","Model","Model Yılı","Otomatik Vites","Yakıt Tipi","Günlük Fiyat");
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","-----------","--------","---------","----------","--------------","----------","------------");
+        for (Audi w :audiList){
+
+            if (w.isActive()==isActive){
+                System.out.printf("%-13s  %-10s  %-16s  %-10s  %-12s  %-12s $%-14s\n",w.getId(),"Honda",w.getModel(),w.getModelYear(),w.isGear(),w.getFuelType(),w.getDailyPrice());
+            }
+        }
+        System.out.println();
+    }
+
+
+    public void showInactiveCars(boolean isActive){
+        System.out.println();
+        System.out.println("////////////////////////////////////// MÜSAIT OLMAYAN ARACLARIMIZ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ \n");
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","Arac Kodu","Marka","Model","Model Yılı","Otomatik Vites","Yakıt Tipi","Günlük Fiyat");
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","-----------","--------","---------","----------","--------------","----------","------------");
+        for (Audi w :audiList){
+
+            if (!w.isActive()){
+                System.out.printf("%-13s  %-10s  %-16s  %-10s  %-12s  %-12s $%-14s\n",w.getId(),"Honda",w.getModel(),w.getModelYear(),w.isGear(),w.getFuelType(),w.getDailyPrice());
+            }
+        }
     }
 }
--- src/entities/concretes/Bmw.java
+++ src/entities/concretes/Bmw.java
@@ -9,27 +9,21 @@ public class Bmw extends Cars {
 
     public List<Bmw> bmwList = new ArrayList<>();
 
-    private String model;
+    //private String model;
 
     public Bmw() {
     }
 
-    public Bmw(String model, String id, int modelYear, boolean gear, String fuelType, double dailyPrice) {
-        this.model = model;
+    public Bmw(String model, String id, int modelYear, boolean gear, String fuelType, double dailyPrice,boolean isActive) {
+        super.setModel(model);
         super.setId(id);
         super.setModelYear(modelYear);
         super.setGear(gear);
         super.setFuelType(fuelType);
         super.setDailyPrice(dailyPrice);
+        super.setActive(isActive);
     }
 
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
 
     @Override
     public String toString() {
@@ -39,34 +33,21 @@ public class Bmw extends Cars {
                         ", Model Year: "+ getModelYear() +
                         ", Automatic Gear: "+ isGear() +
                         ", Fuel Type: " + getFuelType() +
-                        ", Daily Price: " + getDailyPrice()
+                        ", Daily Price: " + getDailyPrice()+
+                        ", Is Active: " + isActive()
                 ;
     }
 
-    @Override
-    public void showAvailableCars(boolean isActive) {
-
-    }
-
-    @Override
-    public void showSelectedCar(boolean isActive) {
-
-    }
-
-    @Override
-    public void showInactiveCars(boolean isActive) {
-
-    }
 
     public void fillBmwList(){
-        Bmw bmw = new Bmw();
-        Bmw bmw2 = new Bmw();
-        Bmw bmw3 = new Bmw();
-        Bmw bmw4 = new Bmw();
-        Bmw bmw5 = new Bmw();
-        Bmw bmw6 = new Bmw();
-        Bmw bmw7 = new Bmw();
-        Bmw bmw8 = new Bmw();
+        Bmw bmw =  new Bmw("118","1182020100",2020,true,"gasoline",75.90,true);
+        Bmw bmw2 = new Bmw("116","1162022101",2022,false,"electric",55.90,true);
+        Bmw bmw3 = new Bmw("216","2162021100",2021,true,"diesel",65.90,true);
+        Bmw bmw4 = new Bmw("218","2182022101",2022,true,"gasoline",65.90,true);
+        Bmw bmw5 = new Bmw("320","3202022100",2022,false,"hybrid",75.90,true);
+        Bmw bmw6 = new Bmw("520","5202023100",2023,true,"gasoline",95.90,true);
+        Bmw bmw7 = new Bmw("X5D","X5D2021100",2021,true,"diesel",105.90,false);
+        Bmw bmw8 = new Bmw("X6B","X6B2020100",2020,true,"electric",125.90,true);
         bmwList.add(bmw);
         bmwList.add(bmw2);
         bmwList.add(bmw3);
@@ -75,16 +56,49 @@ public class Bmw extends Cars {
         bmwList.add(bmw6);
         bmwList.add(bmw7);
         bmwList.add(bmw8);
+
+    }
+    @Override
+    public void showAvailableCars(boolean isActive){
+        System.out.println();
+        System.out.println("////////////////////////////////////// MÜSAIT ARACLARIMIZ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ \n");
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","Arac Kodu","Marka","Model","Model Yılı","Otomatik Vites","Yakıt Tipi","Günlük Fiyat");
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","-----------","--------","---------","----------","--------------","----------","------------");
+        for (Bmw w :bmwList){
+
+            if (w.isActive()==isActive){
+                System.out.printf("%-13s  %-10s  %-16s  %-10s  %-12s  %-12s $%-14s\n",w.getId(),"Honda",w.getModel(),w.getModelYear(),w.isGear(),w.getFuelType(),w.getDailyPrice());
+            }
+        }
+        System.out.println();
     }
 
-    public void showBmwCars(){
+    @Override
+    public void showSelectedCar(boolean isActive) {
+        System.out.println();
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","Arac Kodu","Marka","Model","Model Yılı","Otomatik Vites","Yakıt Tipi","Günlük Fiyat");
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","-----------","--------","---------","----------","--------------","----------","------------");
+        for (Bmw w :bmwList){
 
-        System.out.printf("%-7s  %-10s  %-10s  %-10s  %-14s  %-10s %-15s\n","Marka","Model","ID","Model Yılı","Otomatik Vites","Yakıt Tipi","Günlük Fiyat");
-        System.out.printf("%-7s  %-10s  %-10s  %-10s  %-14s  %-10s %-15s\n","-----","-----","--","----------","--------------","----------","------------");
-        for (Bmw w:bmwList){
-            System.out.printf("%-7s  %-10s  %-10s  %-10s  %-14s  %-10s $%-14s\n","Bmw",w.getModel(),w.getId(),w.getModelYear(),w.isGear(),w.getFuelType(),w.getDailyPrice());
+            if (w.isActive()==isActive){
+                System.out.printf("%-13s  %-10s  %-16s  %-10s  %-12s  %-12s $%-14s\n",w.getId(),"Honda",w.getModel(),w.getModelYear(),w.isGear(),w.getFuelType(),w.getDailyPrice());
+            }
         }
+        System.out.println();
+    }
 
+
+    public void showInactiveCars(boolean isActive){
+        System.out.println();
+        System.out.println("////////////////////////////////////// MÜSAIT OLMAYAN ARACLARIMIZ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ \n");
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","Arac Kodu","Marka","Model","Model Yılı","Otomatik Vites","Yakıt Tipi","Günlük Fiyat");
+        System.out.printf("%-13s  %-10s  %-13s  %-10s  %-14s  %-12s %-15s\n","-----------","--------","---------","----------","--------------","----------","------------");
+        for (Bmw w :bmwList){
+
+            if (!w.isActive()){
+                System.out.printf("%-13s  %-10s  %-16s  %-10s  %-12s  %-12s $%-14s\n",w.getId(),"Honda",w.getModel(),w.getModelYear(),w.isGear(),w.getFuelType(),w.getDailyPrice());
+            }
+        }
     }
 
 }
